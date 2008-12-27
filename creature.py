cdab0026104b9c46ff5a0907e68ba814657141a6
tappi <tappi@c5880b1c-e485-4979-947c-46d9f5173d3f>
2008-12-27 15:02:14 +0000

    changed monster movement to random

--- creature.py
+++ creature.py
@@ -1,9 +1,14 @@
+import random
+
 class Creature:
 	def act(self, game):
-		self.move(game.cur_level.squares[game.player], game.cur_level)
-	
-	def move(self, directionsquare, level):
-		dy, dx = directionsquare.loc
+		#self.move(game.cur_level.squares[game.player].loc, game.cur_level)
+		y,x = game.cur_level.squares[self].loc
+		loc = y + random.randrange(3)-1, x + random.randrange(3)-1
+		self.move(loc, game.cur_level)
+
+	def move(self, loc, level):
+		dy, dx = loc
 		sy, sx = level.squares[self].loc #self loc
 		ny, nx = None, None #newy
 		if dy-sy > 0:
