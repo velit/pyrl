bbdf7ba1ae1be41600bb5737a83917fe598bbf21
tappi <tappi@c5880b1c-e485-4979-947c-46d9f5173d3f>
2009-11-04 19:36:19 +0000

    minor reconstruction

--- char.py
+++ char.py
@@ -1,7 +1,7 @@
-import curses
+from io import IO
 
 class Char:
-	"Character object that holds both the visible character as well as the color that is used with ncurses."
-	def __init__(self, symbol=' ', color=curses.A_NORMAL):
+	"""Character object that holds both the visible character as well as the color that is used with ncurses."""
+	def __init__(self, symbol=' ', color=IO().colors["grey"]):
 		self.symbol = symbol
 		self.color = color
