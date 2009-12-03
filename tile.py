cd6bb2086a6d847776b17b8ef3f263532b84fa87
tappi <tappi@c5880b1c-e485-4979-947c-46d9f5173d3f>
2009-12-03 23:01:39 +0000

    the walls are brown, but fade in memory

--- tile.py
+++ tile.py
@@ -3,19 +3,20 @@ from colors import color
 
 class Tile(object):
 	"""The actual floor of a square."""
-	def __init__(self, name = "Unknown", ch = Char(' '), \
+	def __init__(self, name = "Unknown", visible_ch = Char(' '), memory_ch = Char(' '),
 			passable = False, destroyable = False, see_through = False ):
 		self.name = name
-		self.ch = ch
+		self.ch = visible_ch
+		self.memory_ch = memory_ch
 		self.passable = passable
 		self.destroyable = destroyable
 		self.see_through = see_through
 
 tiles = {
-		"u": Tile("You have not seen this place yet", Char(' '), False, False, False),
-		"f": Tile("Dungeon floor", Char('.'), True, False, True),
-		"r": Tile("Dungeon rock", Char('#'), False, True, False),
-		"w": Tile("Wall", Char('#', color["black"]), False, True, False),
-		"ds": Tile("Down staircase", Char('>'), True, True, True),
-		"us": Tile("Up staircase", Char('<'), True, True, True),
+		"u": Tile("You have not seen this place yet", Char(' '), Char(' '), False, False, False),
+		"f": Tile("Dungeon floor", Char('.'), Char('.'), True, False, True),
+		"r": Tile("Dungeon rock", Char('#'), Char('#'), False, True, False),
+		"w": Tile("Wall", Char('#', color["brown"]), Char('#', color["black"]), False, True, False),
+		"ds": Tile("Down staircase", Char('>'), Char('>'), True, True, True),
+		"us": Tile("Up staircase", Char('<'), Char('<'), True, True, True),
 		}
