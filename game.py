a031e7fc2ec01bcd91a2ffb311291cccff702a60
tappi <tappi@c5880b1c-e485-4979-947c-46d9f5173d3f>
2009-12-07 14:21:56 +0000

    fixed level loading in editor, some refactoring

--- game.py
+++ game.py
@@ -1,19 +1,24 @@
 import curses
 import sys
-import pickle
+import cPickle
+
+from os import path
 
 from level import Level
 from player import Player
 from io import io
 
 class Game(object):
-	def __init__(self):
+	def __init__(self, main):
 		"""The game object for Tapani Kiiskisen's game pyrl made with curses."""
+		self.main = main
+
 		self.turn_counter = 0
 
 		self.l = [Level(1)]
 		self.p = Player(self.l[0], self)
 		self.p.l.addCreature(self.p)
+		self.redraw()
 	
 	def play(self):
 		for creature in self.p.l.creatures:
@@ -29,18 +34,18 @@ class Game(object):
 		else:
 			self.p.l = self.l[self.l.index(self.p.l) + 1]
 			self.p.l.addCreature(self.p, self.p.l.squares["us"])
-		self.p.l.drawMemory()
+		self.redraw()
 
 	def ascend(self):
 		if self.l.index(self.p.l) > 0:
 			self.p.l.removeCreature(self.p)
 			self.p.l = self.l[self.l.index(self.p.l) - 1]
 			self.p.l.addCreature(self.p, self.p.l.squares["ds"])
-			self.p.l.drawMemory()
+			self.redraw()
 		else:
-			self.endGame()
+			self.endgame()
 			
-	def endGame(self, ask=True):
+	def endgame(self, ask=True):
 		if not ask:
 			sys.exit(0)
 		io.queueMsg("Do you wish to end the game? [y/N]:")
@@ -49,17 +54,27 @@ class Game(object):
 			sys.exit(0)
 	
 	def _save(self):
-		f = open("pyrl.svg", "w")
-		pickle.dump(self, f)
+		f = open(path.join("data", "pyrl.svg"), "w")
+		cPickle.dump(self, f)
 		f.close()
 
-	def saveGame(self, ask=True):
+	def savegame(self, ask=True):
 		if not ask:
 			self._save()
-			self.endGame(True)
+			self.endgame(True)
 		io.queueMsg("Do you wish to save the game? [Y/n]:")
 		c = io.getCharacters(map(ord, ('y', 'Y', 'n', 'N', '\n', ' ')))
-		if c in map(ord, ('y', 'Y')):
+		if c in map(ord, ('y', 'Y', '\n', ' ')):
 			self._save()
-			self.endGame(True)
+			self.endgame(True)
 
+	def loadgame(self, ask=True):
+		if not ask:
+			self.main.load()
+		io.msg("Do you wish to load the game? [y/N]:")
+		c = io.getCharacters(map(ord, ('y', 'Y', 'n', 'N', '\n', ' ')))
+		if c in map(ord, ('y', 'Y')):
+			self.main.load()
+
+	def redraw(self):
+		self.p.l.drawMemory()
