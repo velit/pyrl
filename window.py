337051efc52e68754e5f66f1e65ac1663b83acea
Tapani Kiiskinen <tapani.kiiskinen@gmail.com>
2010-07-07 20:27:26 +0000

    horizontal menus, moved menu to own file

--- window.py
+++ window.py
@@ -1,4 +1,6 @@
 import curses
+import menu
+
 from char import Char
 from colors import color
 from constants import YES, NO, DEFAULT
@@ -96,97 +98,5 @@ class Window(object):
 	def _getch(self):
 		return self.w.getch()
 
-	def _print_selection_values(self, i, x, n, v, reverse=True):
-		len_n = len(n[i])
-		if v is not None and i < len(v) and reverse:
-			self.w.addstr(i, len_n, " "*(x-len_n), color["reverse"])
-			if isinstance(v[i], Char):
-				self.w.addstr(i, x, v[i].symbol, v[i].color | color["reverse"])
-			else:
-				self.w.addstr(i, x, v[i], color["reverse"])
-		elif v is not None and i < len(v):
-			self.w.addstr(i, len_n, " "*(x-len_n))
-			if isinstance(v[i], Char):
-				self.w.addstr(i, x, v[i].symbol, v[i].color)
-			else:
-				self.w.addstr(i, x, v[i])
-
-	def _get_indentation_level(self, rows):
-		indent = 0
-		# if lines in rows has second words, find out indentation level
-		ii = isinstance
-		if any(not ii(l, str) for l in rows):
-			for l in rows:
-				if not ii(l, str):
-					if ii(l[0], Char):
-						indent = max(indent, 1)
-					else:
-						indent = max(indent, len(l[0]))
-			indent += 1 #leave room between words
-		return indent
-
-	def _print_selection_word(self, y, x, word, reverse=False):
-		r = color["reverse"] if reverse else color["normal"]
-		if isinstance(word, Char):
-			self.w.addstr(y, x, word.symbol, word.color | r)
-		else:
-			self.w.addstr(y, x, str(word), r)
-
-	def _print_selection_line(self, y, line, indent, reverse=False):
-		if not isinstance(line, str):
-			l = len(line[0])
-			self._print_selection_word(y, 0, line[0], reverse)
-			self._print_selection_word(y, l, " "*(indent-l), reverse)
-			self._print_selection_word(y, indent, line[1], reverse)
-		else:
-			self._print_selection_word(y, 0, line, reverse)
-
-	def _print_selection(self, rows, indent):
-		for y, line in enumerate(rows):
-			self._print_selection_line(y, line, indent)
-
-	def _print_select_getch(self, y, line, indent):
-		self._print_selection_line(y, line, indent, True)
-		c = self.w.getch()
-		self._print_selection_line(y, line, indent, False)
-		return c
-
-	def _roll_i(self, i, n, d, a=0):
-		i += a
-		if i >= len(n):
-			i = 0
-		elif i < 0:
-			i = len(n)-1
-
-		s = a if a else 1
-		while d[i] is None:
-			i += s
-			if i >= len(n):
-				i = 0
-			elif i < 0:
-				i = len(n)-1
-		return i
-
-	def get_selection(self, print_lines, decisions, keys=(), i=0):
-		curses.curs_set(0)
-		self.clear()
-
-		n = print_lines
-		d = decisions
-		indent = self._get_indentation_level(n)
-
-		self._print_selection(n, indent)
-
-		#ui
-		i = self._roll_i(i, n, d)
-		while True:
-			c = self._print_select_getch(i, n[i], indent)
-			if c in keys:
-				return keys[c]
-			elif c in (curses.KEY_DOWN, ord('j')):
-				i = self._roll_i(i, n, d, 1)
-			elif c in (curses.KEY_UP, ord('k')):
-				i = self._roll_i(i, n, d, -1)
-			elif c == ord('\n') or c == ord('>'):
-				curses.curs_set(1)
-				return decisions[i]
+	def draw_menu(self, lines, returns, keys=(), i=0):
+		return menu.draw(self, lines, returns, keys, i)
