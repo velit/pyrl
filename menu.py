f9058b438619fce13b9a1dcd02a2a5ed6eeff337
tappi <tappi@c5880b1c-e485-4979-947c-46d9f5173d3f>
2010-12-25 03:50:37 +0000

    Menu draw improvements, editor menu improvements, line options view can now return the key which selected the line

--- menu.py
+++ menu.py
@@ -3,103 +3,82 @@ import colors
 from char import Char
 from collections import Sequence
 
-def draw(io, lines, returns, keys=(), start_line=0):
-	indent = _get_indent(lines)
-	_print_menu(io, lines, indent)
-	return _update_ui(io, lines, returns, keys, start_line, indent)
+def draw(io, start_line, lines, line_ignores):
+	indt = _get_indent(lines)
+	_print_menu(io, indt, lines)
+	return _update_ui(io, start_line, indt, lines, line_ignores)
 
-def _update_ui(io, l, r, k, sl, i):
-	sl = _roll_sl(sl, l, r)
-	while True:
-		c = _hilight_and_getch(io, sl, l, i)
-		if c in k:
-			curses.curs_set(1)
-			return k[c]
-		elif c in (curses.KEY_DOWN, ord('j')):
-			sl = _roll_sl(sl, l, r, 1)
-		elif c in (curses.KEY_UP, ord('k')):
-			sl = _roll_sl(sl, l, r, -1)
-		elif c == ord('\n') or c == ord('>'):
-			curses.curs_set(1)
-			return r[sl]
+def _get_len(obj):
+	try:
+		return len(obj)
+	except TypeError:
+		return len(str(obj))
 
-def _get_indent(lines, initial_i=0):
-	i = 0
+def _get_indent(lines, column=0):
+	indt = 0
 	# if any line has second words, find out indentation level
-	ii = isinstance
 	for l in lines:
-		if not ii(l, str) and ii(l, Sequence):
+		if not isinstance(l, str):
 			try:
-				b = l[initial_i]
+				o = l[column]
+				indt = max(indt, _get_len(o)+1)
 			except IndexError:
 				continue
 
-			if ii(b, str):
-				i = max(i, len(b)+1)
-			elif ii(b, Char):
-				i = max(i, 1+1)
-			else:
-				i = max(i, len(str(b))+1)
-	return i
+	return indt
 
-	#if any(not ii(l, str) and ii(l, Sequence) for l in lines):
-	#	for l in lines:
-	#		if not ii(l, str) and ii(l, Sequence):
-	#			b = l[0]
-
-	#			if ii(b, str):
-	#				i = max(i, len(b))
-	#			elif ii(b, Char):
-	#				i = max(i, 1)
-	#			else:
-	#				i = max(i, len(str(b)))
-	#		else:
-	#			if ii(l, str):
-	#				i = max(i, len(l))
-	#			elif ii(l, Char):
-	#				i = max(i, 1)
-	#			else:
-	#				i = max(i, len(str(l)))
-
-def _print_menu(io, l, i):
+def _print_menu(io, indt, lines):
 	curses.curs_set(0)
 	io.clear()
-	for y, line in enumerate(l):
-		_print_menu_line(io, y, i, l)
+	for y in range(len(lines)):
+		_print_menu_line(io, y, indt, lines)
 
-def _print_menu_line(io, sl, i, l, reverse=False):
-	r = colors.reverse if reverse else colors.normal
-	if not isinstance(l[sl], str) and isinstance(l[sl], Sequence):
-		ln = len(str(l[sl][0]))
-		_print_menu_word(io, sl, 0, l[sl][0], r)
-		_print_menu_word(io, sl, ln, " "*(i-ln), r)
-		_print_menu_word(io, sl, i, l[sl][1], r)
-	else:
-		_print_menu_word(io, sl, 0, l[sl], r)
+def _update_ui(io, start_line, indt, lines, line_ignores):
+	i = _roll_index(start_line, lines, line_ignores)
+	while True:
+		ch = _hilight_and_getch(io, i, indt, lines)
+		if ch in (curses.KEY_DOWN, ord('j')):
+			i = _roll_index(i, lines, line_ignores, 1)
+		elif ch in (curses.KEY_UP, ord('k')):
+			i = _roll_index(i, lines, line_ignores, -1)
+		else:
+			curses.curs_set(1)
+			return i, ch
 
-def _print_menu_word(io, y, x, word, r):
-	if isinstance(word, Char):
-		io.w.addstr(y, x, word.symbol, colors.d[word.color] | r)
-	else:
-		io.w.addstr(y, x, str(word), r)
-
-def _hilight_and_getch(io, sl, l, i):
-	_print_menu_line(io, sl, i, l, True)
+def _hilight_and_getch(io, line_i, indt, lines):
+	_print_menu_line(io, line_i, indt, lines, True)
 	c = io.w.getch()
-	_print_menu_line(io, sl, i, l, False)
+	_print_menu_line(io, line_i, indt, lines, False)
 	return c
 
-def _roll_sl(sl, l, r, add=0):
-	sl += add
-	#stepper
-	s = -1 if add < 0 else 1
-
+def _roll_index(start_line, lines, ignores, add=0):
+	i = start_line + add
+	direction = add if add else 1
 	while True:
-		if sl >= len(l):
-			sl = 0
-		elif sl < 0:
-			sl = len(l)-1
-		if r[sl] is not None:
+		if i >= len(lines):
+			i = 0
+		if i < 0:
+			i = len(lines)-1
+		if i not in ignores:
 			break
-		sl += s
-	return sl
+		i += direction
+	return i
+
+def _print_menu_line(io, line_i, indt, lines, reverse_color=False):
+	i = line_i
+	line = lines[i]
+	reverse = colors.reverse if reverse_color else colors.normal
+	if not isinstance(line, str) and isinstance(lines, Sequence):
+		ln = _get_len(line[0])
+		_print_menu_word(io, i, 0, line[0], reverse)
+		_print_menu_word(io, i, ln, " "*(indt-ln), reverse)
+		_print_menu_word(io, i, indt, line[1], reverse)
+	else:
+		_print_menu_word(io, i, 0, line, reverse)
+
+def _print_menu_word(io, y, x, word, reverse_color):
+	if isinstance(word, Char):
+		io.w.addstr(y, x, word.symbol, colors.d[word.color] | reverse_color)
+	else:
+		io.w.addstr(y, x, str(word), reverse_color)
+
