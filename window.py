c4d9b95815c61f8407bed9f9e8905e7b59455541
tappi <tappi@c5880b1c-e485-4979-947c-46d9f5173d3f>
2011-01-05 01:09:30 +0000

    added some magical print_wrap that i did some time ago, no idea what it does

--- window.py
+++ window.py
@@ -109,6 +109,37 @@ class Window(object):
 	def addstr(self, *args, **keys):
 		self.w.addstr(*args, **keys)
 
+	def wrap_print(self, words):
+		self.clear()
+		str = words
+		cur_line = 0
+		skip_all = False
+		while True:
+			if cur_line < self.lines - 1:
+				if len(str) < self.width:
+					self.w.addstr(cur_line, 0, str)
+					break
+				else:
+					a = self.wrapper.wrap(str)
+					self.w.addstr(cur_line, 0, a[0])
+					str = " ".join(a[1:])
+					cur_line += 1
+			elif cur_line == self.lines - 1:
+				if len(str) < self.width:
+					self.w.addstr(cur_line, 0, str)
+					break
+				else:
+					a = self.last_line_wrapper.wrap(str)
+					self.w.addstr(cur_line, 0, a[0]+self.more_str)
+					if not skip_all:
+						c = self.getch_from_list(list=(ord('\n'), ord(' ')))
+						if c == ord('\n'):
+							skip_all = True
+					str = " ".join(a[1:])
+					cur_line = 0
+					self.clear()
+		self.msgqueue = ""
+
 	def draw_menu(self, *args, **keys):
 		return menu.draw(self, *args, **keys)
 
