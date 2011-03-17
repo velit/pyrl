bd76a30e71a27e3c3a1765c71acc3d7618627692
Tapani Kiiskinen <tapani.kiiskinen@gmail.com>
2011-03-17 08:59:01 +0200

    added tiles.py back after accidentally deleting it with stash

---
+++ tiles.py
@@ -0,0 +1,34 @@
+from tile import Tile
+from char import Char
+from const.game import PASSAGE_UP, PASSAGE_DOWN
+
+UNKNOWN = "u"
+FLOOR = "f"
+WALL = "w"
+ROCK = "r"
+STAIRS_UP = "us"
+STAIRS_DOWN = "ds"
+
+#TODO: tallenna jsonina ja loadaa gameen/editoriin erikseen
+tiles = {
+	UNKNOWN: Tile("You have not seen this place yet",
+			Char(' '), Char(' '), False, False),
+	FLOOR: Tile("Dungeon floor", Char('.'), Char('.')),
+	ROCK: Tile("Dungeon rock", Char('#', "black"), Char('#', "black"),
+			False, False),
+	WALL: Tile("Wall", Char('#', "brown"), Char('#', "black"),
+			False, False),
+	STAIRS_DOWN: Tile("Down staircase", Char('>', "red"), Char('>'),
+			exit_point=PASSAGE_DOWN),
+	STAIRS_UP: Tile("Up staircase", Char('<', "red"), Char('<'),
+			exit_point=PASSAGE_UP),
+}
+
+def gettile(handle, tile_dict=None):
+	if tile_dict is not None and handle in tile_dict:
+		return tile_dict[handle]
+	elif handle in tiles:
+		return tiles[handle]
+	else:
+		raise KeyError("Handle '{}' not in global tiles or '{}'".format(handle,
+			tile_dict))
