0d96a7cb4334292b7360fbd907a494ef643f94fc
Tapani Kiiskinen <tapani.kiiskinen@gmail.com>
2012-02-11 02:46:17 +0200

    more super() removals, deleted window(>PyrlWindow)

--- window/pyrl_window.py
+++ window/pyrl_window.py
@@ -21,15 +21,6 @@ class PyrlWindow:
 	def addstr(self, string, color=None):
 		self._w.addstr(string, color)
 
-	def clear(self):
-		self._w.clear()
-
-	def prepare_flush(self):
-		self._w.prepare_flush()
-
-	def flush(self):
-		self._w.flush()
-
 	def move(self, y, x):
 		self._w.move(y, x)
 
@@ -41,6 +32,15 @@ class PyrlWindow:
 
 	def get_dimensions(self):
 		return self._w.get_dimensions()
+
+	def clear(self):
+		self._w.clear()
+
+	def prepare_flush(self):
+		self._w.prepare_flush()
+
+	def flush(self):
+		self._w.flush()
 
 
 	# PyrlWindow added functions
@@ -56,37 +56,6 @@ class PyrlWindow:
 		while c not in char_list:
 			c = self.getch()
 		return c
-
-	def wrap_print(self, words):
-		self.clear()
-		str = words
-		cur_line = 0
-		skip_all = False
-		while True:
-			if cur_line < self.lines - 1:
-				if len(str) < self.width:
-					self.addstr(cur_line, 0, str)
-					break
-				else:
-					a = self.wrapper.wrap(str)
-					self.addstr(cur_line, 0, a[0])
-					str = " ".join(a[1:])
-					cur_line += 1
-			elif cur_line == self.lines - 1:
-				if len(str) < self.width:
-					self.addstr(cur_line, 0, str)
-					break
-				else:
-					a = self.last_line_wrapper.wrap(str)
-					self.addstr(cur_line, 0, a[0] + self.more_str)
-					if not skip_all:
-						c = self.getch_from_list(list=(ord('\n'), ord(' ')))
-						if c == ord('\n'):
-							skip_all = True
-					str = " ".join(a[1:])
-					cur_line = 0
-					self.clear()
-		self.msgqueue = ""
 
 	def draw_menu(self, *a, **k):
 		return menu.draw(self, *a, **k)
