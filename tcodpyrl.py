8e18b8d39a6147648f9ea402b3c1976f5a3680fe
Tapani Kiiskinen <tapani.kiiskinen@gmail.com>
2012-02-12 13:48:59 +0200

    split ./pyrl.py --libtcod, added a msg debug func

--- tcodpyrl.py
+++ tcodpyrl.py
@@ -11,12 +11,7 @@ parser.add_argument(u"-p", u"--profile", action=u"store_true")
 
 options = parser.parse_args()
 
-try:
-	import curses
-except ImportError:
-	print("Couldn't import curses. Try the libtcod version with ./tcodpyrl.py")
-
 if options.profile:
-	cProfile.run(u"curses.wrapper(main.curses_inited_main, options)", u"profiler_data")
+	cProfile.run(u"main.tcod_main(options)", u"profiler_data")
 else:
-	curses.wrapper(main.curses_inited_main, options)
+	main.tcod_main(options)
