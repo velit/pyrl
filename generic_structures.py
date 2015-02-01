71046b32bee905f045ccd4749c660ef624d72eef
Tapani Kiiskinen <tapani.kiiskinen@gmail.com>
2015-02-01 11:32:21 +0200

    Made TwoDimensionalFlatList more generic when it comes to dimensions.

--- generic_structures.py
+++ generic_structures.py
@@ -1,23 +1,29 @@
 
 
-class TwoDimensionalList(list):
+class TwoDimensionalFlatList(list):
 
     """
-    Dynamic two dimensional list with a bounded width.
+    Flat list with two-dimensional acessor methods.
 
-    Uses coordinates in _list[y, x] fashion.
+    The second dimension is bounded by constructor parameter, first dimension is
+    bounded by the amount of items given to the list divided by the second
+    dimension bound.
+
+    Iterating the flat list gives items that increase quicker in the second dimension. In other
+    words the first four items in a list with the second dimension bounded to
+    two are: (0, 0), (0, 1), (1, 0), (1, 1).
     """
 
-    def __init__(self, iterable, width):
+    def __init__(self, iterable, second_dimension_bound):
         list.__init__(self, iterable)
-        self._width = width
+        self._second_dim_bound = second_dimension_bound
 
     def _get_index(self, coord):
-        y, x = coord
-        if x < self._width:
-            return y * self._width + x
+        first_dim, second_dim = coord
+        if second_dim < self._second_dim_bound:
+            return first_dim * self._second_dim_bound + second_dim
         else:
-            raise IndexError("list x-component out of range.")
+            raise IndexError("second dimension index out of range.")
 
     def __getitem__(self, coord):
         return list.__getitem__(self, self._get_index(coord))
