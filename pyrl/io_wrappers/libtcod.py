ee2798088899db29ed619d38d5ace1e86e756980
Tapani Kiiskinen <tapani.kiiskinen@gmail.com>
2021-01-09 05:11:35 +0200

    Remove libtcod as a static dependency
    Now only tcod remains as a dynamic dependency

--- pyrl/io_wrappers/libtcod.py
+++ pyrl/io_wrappers/libtcod.py
@@ -1,11 +1,10 @@
+import tcod
+
 from pyrl.config.config import Config
 from pyrl.enums.colors import Pair, Color
 from pyrl.enums.keys import Key
 from pyrl.io_wrappers.libtcod_dicts import libtcod_color_map, libtcod_key_map
-from pyrl.io_wrappers.libtcod_import import import_libtcod
 from pyrl.window.window_system import WindowSystem
-
-libtcod = import_libtcod()
 
 IMPLEMENTATION = "tcod"
 
@@ -17,19 +16,19 @@ class TCODWrapper:
 
     def __init__(self, root_window=None):
         """Init the SDL surface and prepare for draw calls."""
-        flags = libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_ASCII_INROW
-        libtcod.console_set_custom_font(b"resources/terminal10x18_gs_ro.png", flags)
+        flags = tcod.FONT_TYPE_GREYSCALE | tcod.FONT_LAYOUT_ASCII_INROW
+        tcod.console_set_custom_font(b"resources/terminal10x18_gs_ro.png", flags)
         rows, cols = WindowSystem.game_dimensions
-        libtcod.console_init_root(cols, rows, Config.default_game_name.encode(), False,
-                                  libtcod.RENDERER_SDL)
+        tcod.console_init_root(cols, rows, Config.default_game_name.encode(), False,
+                               tcod.RENDERER_SDL)
 
     def new_window(self, dimensions):
         rows, columns = dimensions
-        window = libtcod.console_new(columns, rows)
+        window = tcod.console_new(columns, rows)
         return TCODWindow(window)
 
     def flush(self):
-        libtcod.console_flush()
+        tcod.console_flush()
 
     def suspend(self):
         """SDL version doesn't require suspend."""
@@ -40,10 +39,10 @@ class TCODWrapper:
         pass
 
     def _toggle_fullscreen(self):
-        if libtcod.console_is_fullscreen():
-            libtcod.console_set_fullscreen(False)
+        if tcod.console_is_fullscreen():
+            tcod.console_set_fullscreen(False)
         else:
-            libtcod.console_set_fullscreen(True)
+            tcod.console_set_fullscreen(True)
 
 class TCODWindow:
 
@@ -55,28 +54,28 @@ class TCODWindow:
         self.default_fg = self.color_map[Color.Normal]
         self.default_bg = self.color_map[Color.Black]
         self.win = libtcod_window
-        libtcod.console_set_default_foreground(self.win, self.default_fg)
-        libtcod.console_set_default_background(self.win, self.default_bg)
+        tcod.console_set_default_foreground(self.win, self.default_fg)
+        tcod.console_set_default_background(self.win, self.default_bg)
 
     def get_key(self):
         while True:
-            key_event = libtcod.Key()
-            mouse_event = libtcod.Mouse()
-            libtcod.sys_wait_for_event(libtcod.EVENT_KEY_PRESS, key_event, mouse_event, False)
+            key_event = tcod.Key()
+            mouse_event = tcod.Mouse()
+            tcod.sys_wait_for_event(tcod.EVENT_KEY_PRESS, key_event, mouse_event, False)
             key = self._interpret_event(key_event)
             if key != Key.NO_INPUT:
                 return key
 
     def check_key(self):
-        event = libtcod.console_check_for_keypress(libtcod.KEY_PRESSED)
+        event = tcod.console_check_for_keypress(tcod.KEY_PRESSED)
         return self._interpret_event(event)
 
     def _interpret_event(self, event):
-        if libtcod.console_is_window_closed():
+        if tcod.console_is_window_closed():
             return Key.CLOSE_WINDOW
         elif event.vk in self.key_map:
             return self.key_map[event.vk]
-        elif event.vk == libtcod.KEY_CHAR:
+        elif event.vk == tcod.KEY_CHAR:
             if event.c == ord('c') and event.lctrl or event.rctrl:
                 raise KeyboardInterrupt
             ch = chr(event.c)
@@ -91,20 +90,20 @@ class TCODWindow:
             return Key.NO_INPUT
 
     def clear(self):
-        libtcod.console_clear(self.win)
+        tcod.console_clear(self.win)
 
     def blit(self, size, screen_position):
         rows, cols = size
         y, x = screen_position
-        libtcod.console_blit(self.win, 0, 0, cols, rows, 0, x, y, 1.0, 1.0)
+        tcod.console_blit(self.win, 0, 0, cols, rows, 0, x, y, 1.0, 1.0)
 
     def get_dimensions(self):
-        return libtcod.console_get_height(self.win), libtcod.console_get_width(self.win)
+        return tcod.console_get_height(self.win), tcod.console_get_width(self.win)
 
     def draw_char(self, char, coord=(0, 0)):
         y, x = coord
         symbol, (fg, bg) = char
-        libtcod.console_put_char_ex(self.win, x, y, symbol, self.color_map[fg], self.color_map[bg])
+        tcod.console_put_char_ex(self.win, x, y, symbol, self.color_map[fg], self.color_map[bg])
 
     def draw_str(self, string, coord=(0, 0), color=None):
         y, x = coord
@@ -112,18 +111,18 @@ class TCODWindow:
             fg, bg = Pair.Normal
         else:
             fg, bg = color
-        libtcod.console_set_color_control(libtcod.COLCTRL_1, self.color_map[fg], self.color_map[bg])
-        string = chr(libtcod.COLCTRL_1) + string + chr(libtcod.COLCTRL_STOP)
-        libtcod.console_print(self.win, x, y, string)
+        tcod.console_set_color_control(tcod.COLCTRL_1, self.color_map[fg], self.color_map[bg])
+        string = chr(tcod.COLCTRL_1) + string + chr(tcod.COLCTRL_STOP)
+        tcod.console_print(self.win, x, y, string)
 
     def draw(self, char_payload_sequence):
-        d = libtcod.console_put_char_ex
+        d = tcod.console_put_char_ex
         local_color = self.color_map
         for (y, x), (symbol, (fg, bg)) in char_payload_sequence:
             d(self.win, x, y, symbol, local_color[fg], local_color[bg])
 
     def draw_reverse(self, char_payload_sequence):
-        d = libtcod.console_put_char_ex
+        d = tcod.console_put_char_ex
         local_color = self.color_map
         for (y, x), (symbol, (fg, bg)) in char_payload_sequence:
             d(self.win, x, y, symbol, local_color[bg], local_color[fg])
